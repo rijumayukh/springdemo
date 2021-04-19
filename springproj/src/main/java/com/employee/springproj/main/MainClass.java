1956793faad02eb632b00f9361f2dd7b02dd63b7
Riju <mayukh.riju@gmail.com>
2021-04-19 17:17:14 +0530

    Adding core spring initial project

--- springproj/src/main/java/com/employee/springproj/main/MainClass.java
+++ springproj/src/main/java/com/employee/springproj/main/MainClass.java
@@ -1,13 +1,18 @@
 package com.employee.springproj.main;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.employee.springproj.service.EmployeeServiceImpl;
+
 public class MainClass 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+        		"applicationContext.xml");
+        EmployeeServiceImpl EmployeeServiceImpl = context.getBean("EmployeeService", EmployeeServiceImpl.class);
+        EmployeeServiceImpl.getEmployee(123L);
     }
 }
---
+++ springproj/src/main/java/com/employee/springproj/service/EmployeeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.employee.springproj.service;
+
+public class EmployeeServiceImpl {
+
+	public void getEmployee(Long empId){
+		System.out.println("get employee called");
+	}
+}
