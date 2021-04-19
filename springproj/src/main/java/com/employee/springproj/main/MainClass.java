b4cc70027ff15a5aec1788053c2c556d9fe432b8
Riju <mayukh.riju@gmail.com>
2021-04-19 15:59:36 +0530

    first commit

---
+++ springproj/src/main/java/com/employee/springproj/main/MainClass.java
@@ -0,0 +1,13 @@
+package com.employee.springproj.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
